02b8d795744bdbfd10265fa7196b3ece33061bf9
Antoine Petit <antoine.petit4723@gmail>
2023-02-08 20:00:03 +0100

    premier commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DxDataGridModule } from 'devextreme-angular';
+import { NavbarComponent } from './components/navbar/navbar/navbar.component';
+import { EmployeeComponent } from './components/employee/employee/employee.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    EmployeeComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    DxDataGridModule
   ],
   providers: [],
   bootstrap: [AppComponent]
